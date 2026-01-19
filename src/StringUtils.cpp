5214bd25327a9d04005578a895cd02eb6b848f9a
Bingyuan Cai <cbycai@ucdavis.edu>
2026-01-19 00:52:24 +0000

    This is change for complete slice

--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -4,7 +4,53 @@ namespace StringUtils{
 
 std::string Slice(const std::string &str, ssize_t start, ssize_t end) noexcept{
     // Replace code here
-    return "";
+    std::string Temp = str;
+    ssize_t n = str.size();
+    // empty str
+    if (n==0){
+        return "";
+    }
+
+    //end == 0
+    if(end==0){
+        end = n;
+    }
+
+    //start is negative
+    if(start<0){
+        start = n+start;
+        //That indicates start is too negative
+        if (start<0){
+            start = 0;
+        }
+    }
+
+    //end is negative
+    if(end<0){
+        end = end+n;
+        // if end is too small there is no slice in range
+        if(end<0){
+            return "";
+        }
+    }
+
+    //start is greater and equal to length
+    if(start>=n){
+        return "";
+    }
+
+    //end is greater than length and I discuss "equal"situation in end==0
+    if(end>n){
+        end = n;
+    }
+    
+    //start>=end
+    if(start>=end){
+        return "";
+    }
+
+    //legal range
+    return str.substr(start, end-start);
 }
 
 std::string Capitalize(const std::string &str) noexcept{
--- testsrc/StringUtilsTest.cpp
+++ testsrc/StringUtilsTest.cpp
@@ -2,8 +2,62 @@
 #include "StringUtils.h"
 
 TEST(StringUtilsTest, SliceTest){
-    
+    //basic but end != 0
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, 1), std::string("a"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 1, 4), std::string("bcd"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, 6), std::string("abcdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 2, 5), std::string("cde"));
+    //end = 0 means to the end!!!
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, 0), std::string("abcdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 1, 0), std::string("bcdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 2, 0), std::string("cdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 3, 0), std::string("def"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 4, 0), std::string("ef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 5, 0), std::string("f"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 6, 0), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 7, 0), std::string(""));
+    //negative start value
+    EXPECT_EQ(StringUtils::Slice("abcdef", -1, 0), std::string("f"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -2, 0), std::string("ef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -3, 0), std::string("def"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -4, 0), std::string("cdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -5, 0), std::string("bcdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -6, 0), std::string("abcdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -7, 0), std::string("abcdef"));
+    //negative end
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, -1), std::string("abcde"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, -2), std::string("abcd"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, -5), std::string("a"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, -6), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, -9), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 1, -1), std::string("bcde"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -4, -1), std::string("cde"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -6, -5), std::string("a"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -7, -5), std::string("a"));
+    //large range
+    EXPECT_EQ(StringUtils::Slice("abcdef", -100, 2), std::string("ab"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 2, 100), std::string("cdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", -100, 100), std::string("abcdef"));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 100, 0), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 100, 200), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 0, -100), std::string(""));
+    //start>=end
+    EXPECT_EQ(StringUtils::Slice("abcdef",3, 3), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("abcdef", 4, 3), std::string(""));
+    //empty string
+    EXPECT_EQ(StringUtils::Slice("", 0, 0), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("", 0, 3), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("", -1, 0), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("", -100, 100), std::string(""));
+    //one char
+    EXPECT_EQ(StringUtils::Slice("a", 0, 1), std::string("a"));
+    EXPECT_EQ(StringUtils::Slice("a", 0, 0), std::string("a"));
+    EXPECT_EQ(StringUtils::Slice("a", 1, 0), std::string(""));
+    EXPECT_EQ(StringUtils::Slice("a", -1, 0), std::string("a"));
+    EXPECT_EQ(StringUtils::Slice("a", 0, -1), std::string(""));
 }
+
+
 
 TEST(StringUtilsTest, Capitalize){
     EXPECT_EQ(StringUtils::Capitalize(""), std::string(""));
@@ -27,7 +81,6 @@ TEST(StringUtilsTest, LStrip){
     EXPECT_EQ(StringUtils::LStrip("Hello "), std::string("Hello "));
     EXPECT_EQ(StringUtils::LStrip("   Hello"), std::string("Hello"));
     EXPECT_EQ(StringUtils::LStrip(" \r \t \nHello"), std::string("Hello"));
-
 }
 
 TEST(StringUtilsTest, RStrip){
@@ -50,7 +103,6 @@ TEST(StringUtilsTest, Strip){
     EXPECT_EQ(StringUtils::Strip("Hello   "),std::string("Hello"));
     EXPECT_EQ(StringUtils::Strip("Hello \r \t \n"),std::string("Hello"));
     EXPECT_EQ(StringUtils::Strip(" \r \t \nHello \r \t \n"),std::string("Hello"));
-    
 }
 
 TEST(StringUtilsTest, Center){
